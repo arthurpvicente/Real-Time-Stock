faf4e55d009db2b18889c5bf8ac54440600527ab
arthurpvicente <arthur.p.v@hotmail.com>
2026-04-20 14:55:09 -0500

    fix: rename app title to a better one

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Signalist",
+  title: "Stock Market Tracker",
   description: "Track real-time stock prices, get personalized alerts and explore detailed company insights.",
 };
 
